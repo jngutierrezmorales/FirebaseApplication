f7324d03be245b9491d3a2e3a1aced0dc18cf43b
Juan Gutierrez Morales <jngutierrezmorales@gmail>
2022-02-20 20:51:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Login and Signup with Firebase
+
+Add your google-services.json in app project folder
---
+++ app/src/main/java/com/example/firebaseapplication/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.firebaseapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var loginGoRegisterButton: Button
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        loginEmail = findViewById(R.id.loginEmail)
+        loginPassword = findViewById(R.id.loginPassword)
+        loginButton = findViewById(R.id.loginButton)
+        loginGoRegisterButton = findViewById(R.id.loginGoRegisterButton)
+
+        loginButton.setOnClickListener {
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+
+            if(checkEmpty(email, password)) {
+                login(email, password)
+            }
+        }
+
+        loginGoRegisterButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(applicationContext, "Login failed!", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun checkEmpty(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+}
